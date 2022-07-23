fab731ecf9c837ece81804ef59fa1d1d2bf4008f
MarioCela <100533038+mariohken@users.noreply.github.com>
2022-07-23 19:11:23 +0200

    Getting closer to the requirements
    Added the first insertion of the words of the dictionary, added command +nuova_partita, added "try to guess" part

--- word_checker/main.c
+++ word_checker/main.c
@@ -29,27 +29,50 @@ int create_node();
 //method to print all the words saved in the list
 void print_dictionary();
 
+void new_game();
+
 //TODO has to been modified to correctly elaborate res
-void compute_res(char r[], char p[], char res[]);
+int compute_res(char r[], char p[], char res[]);
 
 int main() {
+    int program_is_up = 1;
+
+    printf("Welcome to WordChecker game. Follow initial instructions and then start playing.\n"
+           "To start a new game insert +nuova_partita, followed by the word that has to be guessed,\n"
+           "the number of attempts you have to guess it and then you can insert the attempts, \n"
+           "each of them followed by the correspondent output\n\n");
+
+    //reading length of the words and the words that will form the initial dictionary
     read_k();
     get_the_starting_dictionary();
 
+    //printing actual dictionary
     print_dictionary();
 
-    char *reference = dictionary_head->word;
-    char word[k];
-    char result[k];
+    //managing a game
+    while (program_is_up) {
+        char* ins = malloc(15);
 
-    printf("Insert second word: ");
-    scanf("%s", word);
+        do {
+            printf("Start a new game inserting +nuova_partita\n");
+            scanf("%s", ins);
+        } while (strcmp(ins, "+nuova_partita") != 0);
 
-    compute_res(reference, word, result);
+        free(ins);
 
-    printf("%s\n", reference);
-    printf("%s\n", word);
-    printf("%s\n", result);
+        new_game();
+
+        ins = malloc(15);
+
+        printf("Game is over, to start a new game insert +nuova_partita, to close the program insert +game_over: ");
+        scanf("%s", ins);
+
+        if (strcmp(ins, "+game_over") == 0) {
+            program_is_up = 0;
+        }
+
+        free(ins);
+    }
 }
 
 void read_k() {
@@ -118,16 +141,55 @@ void print_dictionary() {
     printf("\n");
 }
 
-void compute_res(char* r, char* p, char res[]) {
+void new_game() {
+    int attempts;
+
+    printf("Insert the number of attempts: ");
+    scanf("%d", &attempts);
+
+    char *ref = malloc(k);
+
+    printf("Insert the reference word, the one that has to be guessed: ");
+    scanf("%s", ref);
+
+    for (int i = 0; i < attempts; ++i) {
+        char *word = malloc(k);
+        char *result = malloc(k);
+
+        printf("Try to guess the word (tentative n. %d): ", i + 1);
+        scanf("%s", word);
+
+        compute_res(ref, word, result);
+
+        printf("Output: %s\n\n", result);
+
+        free(word);
+        free(result);
+    }
+}
+
+/**
+ * The method generates the output string depending on the string r and p received as params
+ *
+ * @param r is the reference word that has to be guessed
+ * @param p is the word that tries to guess r
+ * @param res is the resulting string
+ * @return 1 if the word has been guessed, 0 if not
+ */
+int compute_res(char* r, char* p, char res[]) {
     int i;
+    int result = 1;
 
     for (i = 0; i < k - 1; ++i) {
         if (r[i] == p[i]) {
             res[i] = '+';
         } else {
             res[i] = '-';
+            result = 0;
         }
     }
 
     res[i] = '\0';
+
+    return result;
 }
