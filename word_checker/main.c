e2376359cbb1d5b9f5e86466dc5d6bd88200e9c0
MarioCela <100533038+mariohken@users.noreply.github.com>
2022-07-23 17:09:08 +0200

    Reading words and saving them into the list

--- word_checker/main.c
+++ word_checker/main.c
@@ -2,25 +2,41 @@
 #include <string.h>
 #include <malloc.h>
 
+//structure for the nodes where dictionary's words will be saved
 typedef struct {
     char *word;
     struct Node *next;
 } Node;
 
+//length of the words that will be inserted by input
 int k;
-Node *dictionary_head;
 
+//head of the list of nodes of the dictionary
+Node *dictionary_head = NULL;
+
+//method to read from input the value of the length of the words that will be inserted
 void read_k();
 
-void get_the_dictionary();
+//method to initialize the dictionary at the beginning of the program
+void get_the_starting_dictionary();
 
-void create_node(char insert[]);
+//method to insert words to the dictionary
+void insert_words_to_the_dictionary();
 
+//method to create a new node of the list where a word will be saved
+int create_node();
+
+//method to print all the words saved in the list
+void print_dictionary();
+
+//TODO has to been modified to correctly elaborate res
 void compute_res(char r[], char p[], char res[]);
 
 int main() {
     read_k();
-    get_the_dictionary();
+    get_the_starting_dictionary();
+
+    print_dictionary();
 
     char *reference = dictionary_head->word;
     char word[k];
@@ -43,39 +59,63 @@ void read_k() {
     k = ins + 1;
 }
 
-void get_the_dictionary() {
-    char *insertion = malloc(sizeof(char) * k);
+void get_the_starting_dictionary() {
+    insert_words_to_the_dictionary();
+}
 
-    printf("Insert a new word to the dictionary. Insert /quit instead if you do not want to insert any other word: ");
-    scanf("%s", insertion);
+void insert_words_to_the_dictionary() {
+    int check = 1;
 
-    char quit[6];
-    strcpy(quit, "/quit");
-
-    while (strcmp(insertion, quit) != 0) {
-        create_node(insertion);
-        printf("Insert a new word to the dictionary. Insert /quit instead if you do not want to insert any other word: ");
-        scanf("%s", insertion);
+    while (check) {
+        check = create_node();
     }
 }
 
-void create_node(char insert[]) {
-    if (dictionary_head == NULL) {
-        dictionary_head = malloc(sizeof(Node));
-        dictionary_head->word = malloc(sizeof(char) * strlen(insert));
-        strcpy(dictionary_head->word, insert);
-        dictionary_head->next = NULL;
+int create_node() {
+    char *insertion = malloc(sizeof(char) * k);
+    printf("Insert a new word to the dictionary. Insert +inserisci_fine instead if you do not want to insert any other word: ");
+    scanf("%s", insertion);
+
+    if (strcmp(insertion, "+inserisci_fine") != 0) {
+        Node *new_node = malloc(sizeof(Node));
+        new_node->word = malloc(sizeof(char) * strlen(insertion));
+        new_node->word = insertion;
+        new_node->next = NULL;
+
+        if (dictionary_head == NULL) {
+            dictionary_head = new_node;
+        }
+        else {
+            Node *act = dictionary_head;
+            Node *prev;
+            while (act != NULL) {
+                prev = act;
+                act = act->next;
+            }
+            act = malloc(sizeof(Node));
+            act->word = malloc(sizeof(char) * strlen(insertion));
+            strcpy(act->word, insertion);
+            prev->next = act;
+
+            return 1;
+        }
     }
     else {
-        Node *tmp = dictionary_head;
-        while (tmp->next != NULL) {
-            tmp = tmp->next;
-        }
-        tmp->next = malloc(sizeof(Node));
-        tmp->word = malloc(sizeof(char) * strlen(insert));
-        strcpy(tmp->word, insert);
-        tmp->next = NULL;
+        return 0;
     }
+}
+
+void print_dictionary() {
+    Node *tmp = dictionary_head;
+
+    printf("\nPrinting the dictionary:\n");
+
+    do {
+        printf("%s\n", tmp->word);
+        tmp = tmp->next;
+    } while (tmp != NULL);
+
+    printf("\n");
 }
 
 void compute_res(char* r, char* p, char res[]) {
