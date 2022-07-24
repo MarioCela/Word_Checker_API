013d51e67698c2237908d846e581c1c8039c43b6
MarioCela <100533038+mariohken@users.noreply.github.com>
2022-07-24 16:43:08 +0200

    Comments and output
    Added all the commands of the methods and added the correct output depending on the word inserted to guess the reference.
    TODO: compute_res and +stampa_filtrate

--- word_checker/main.c
+++ word_checker/main.c
@@ -29,14 +29,21 @@ int create_node();
 //method to print all the words saved in the list
 void print_dictionary();
 
+//method that manages a game
 void new_game();
 
 //TODO has to been modified to correctly elaborate res
 int compute_res(char r[], char p[], char res[]);
 
+//method launched every time that a new game could start after another one finishes
 int start_new_game();
 
+//method that defines the behavior depending on the user's input. Cases are to start a new game,
+//to close the program and to add new words to the dictionary
 int manage_input_new_game(int *p);
+
+//method that checks if a word inserted by the user to try to guess the reference word belongs to the dictionary or not
+int check_if_belongs_to_the_dictionary(char *word);
 
 int main() {
     int program_is_up;
@@ -46,13 +53,12 @@ int main() {
            "the number of attempts you have to guess it and then you can insert the attempts, \n"
            "each of them followed by the correspondent output\n\n");
 
-    //reading length of the words and the words that will form the initial dictionary
     read_k();
     get_the_starting_dictionary();
 
-    //printing actual dictionary
     print_dictionary();
 
+    //starting first game
     char* ins = malloc(15);
 
     do {
@@ -62,43 +68,67 @@ int main() {
 
     free(ins);
 
+    //launching a game and then, when the latter finishes, asking the user if he wants to close the program, to launch
+    //a new game or to insert new words to the dictionary
     do {
         new_game();
         program_is_up = start_new_game();
     } while (program_is_up);
 }
 
+/**
+ * The method read from the user the length of the words
+ */
 void read_k() {
     int ins;
-    printf("Insert the length of the words: ");
+
+    printf("Insert the length of the words.\n"
+           "Input: ");
     scanf("%d", &ins);
+
+    printf("\n");
+
     k = ins + 1;
 }
 
+/**
+ * The method is called when the program is run first and collects the words inserted by the user
+ */
 void get_the_starting_dictionary() {
     insert_words_to_the_dictionary();
 }
 
+/**
+ * The method is called every time words have to be added to the dictionary
+ */
 void insert_words_to_the_dictionary() {
     int check = 1;
 
     while (check) {
         check = create_node();
-        printf("Dictionary head: %s\n", dictionary_head->word);
     }
 }
 
+/**
+ * The method creates a node in the list representing the dictionary if the user inserts a word and not the ending command.
+ * Every word is added to the correct position so the list is already ordered
+ *
+ * @return 0 if the last input from user was +inserisci_fine, so there is no more need to ask for other words, 1 if not
+ */
 int create_node() {
+    //getting the input
     char *insertion = malloc(sizeof(char) * k);
     printf("Insert a new word to the dictionary. Insert +inserisci_fine instead if you do not want to insert any other word: ");
     scanf("%s", insertion);
 
     if (strcmp(insertion, "+inserisci_fine") != 0) {
+        //creating the new node where the inserted word will be saved
         Node *new_node = malloc(sizeof(Node));
         new_node->word = malloc(sizeof(char) * strlen(insertion));
         new_node->word = insertion;
         new_node->next = NULL;
 
+        //if dictionary head is NULL it means that we are saving the first word of the dictionary
         if (dictionary_head == NULL) {
             dictionary_head = new_node;
         }
@@ -107,10 +137,14 @@ int create_node() {
             Node *prev = NULL;
             int has_to_be_ordered = 1;
 
+            //we start from the first node to control which is the correct position
+            //of the new word to have the list still ordered
             while (act != NULL && has_to_be_ordered) {
                 for (int i = 0; i < k; ++i) {
                     if (new_node->word[i] < act->word[i]) {
                         new_node->next = act;
+
+                        //if prev is NULL it means that the new word will be the new head
                         if (prev == NULL) {
                             dictionary_head = new_node;
                         } else {
@@ -119,15 +153,19 @@ int create_node() {
                         i = k;
                         has_to_be_ordered = 0;
                     }
+                    //if this is not the correct position it is useless to check the remaining positions,
+                    //so the for will end
                     else if (new_node->word[i] > act->word[i]) {
                         i = k;
                     }
                 }
 
+                //going on with the list
                 prev = act;
                 act = act->next;
             }
 
+            //the condition will be true only if the new node will be the last node
             if (has_to_be_ordered) {
                 act = malloc(sizeof(Node));
                 act->word = malloc(sizeof(char) * strlen(insertion));
@@ -143,6 +181,9 @@ int create_node() {
     }
 }
 
+/**
+ * The method prints the dictionary
+ */
 void print_dictionary() {
     Node *tmp = dictionary_head;
 
@@ -156,17 +197,26 @@ void print_dictionary() {
     printf("\n");
 }
 
+/**
+ * The method is launched when a new game starts.
+ * Are asked: the reference word, the number of attempts to try to guess the reference word, and the effective attempts
+ */
 void new_game() {
-    int attempts;
-
-    printf("Insert the number of attempts: ");
-    scanf("%d", &attempts);
-
     char *ref = malloc(k);
 
-    printf("Insert the reference word, the one that has to be guessed: ");
+    //asking for the reference word
+    printf("\nInsert the reference word, the one that has to be guessed: ");
     scanf("%s", ref);
 
+    int attempts;
+
+    //asking for the number of attempts
+    printf("\nInsert the number of attempts: ");
+    scanf("%d", &attempts);
+
+    printf("\n");
+
+    //asking for the words to guess the word
     for (int i = 0; i < attempts; ++i) {
         char *word = malloc(k);
         char *result = malloc(k);
@@ -174,9 +224,15 @@ void new_game() {
         printf("Try to guess the word (tentative n. %d): ", i + 1);
         scanf("%s", word);
 
-        compute_res(ref, word, result);
+        int r = compute_res(ref, word, result);
 
-        printf("Output: %s\n\n", result);
+        if (r == 0) {
+            printf("Output: %s\n\n", result);
+        } else if (r == 1) {
+            printf("Output: ok\n\n");
+        } else {
+            printf("Output: not exist\n\n");
+        }
 
         free(word);
         free(result);
@@ -184,29 +240,37 @@ void new_game() {
 }
 
 /**
+ * /TODO to be modified
+ *
  * The method generates the output string depending on the string r and p received as params
  *
  * @param r is the reference word that has to be guessed
  * @param p is the word that tries to guess r
  * @param res is the resulting string
- * @return 1 if the word has been guessed, 0 if not
+ * @return 1 if the word has been guessed, 0 if not, 2 if the word inserted is not in the dictionary
  */
 int compute_res(char* r, char* p, char res[]) {
-    int i;
-    int result = 1;
+    if (!check_if_belongs_to_the_dictionary(p)) {
+        return 2;
+    } else {
+        int i;
+        int result = 1;
 
-    for (i = 0; i < k - 1; ++i) {
-        if (r[i] == p[i]) {
-            res[i] = '+';
-        } else {
-            res[i] = '-';
-            result = 0;
+        for (i = 0; i < k - 1; ++i) {
+            if (r[i] == p[i]) {
+                res[i] = '+';
+            } else {
+                res[i] = '-';
+                result = 0;
+            }
         }
+
+        res[i] = '\0';
+
+        return result;
     }
 
-    res[i] = '\0';
 
-    return result;
 }
 
 /**
@@ -225,6 +289,13 @@ int start_new_game() {
     return game_goes_on;
 }
 
+/**
+ * The method manages the input by the player, that can choose to start a new game, to close the program, or to
+ * insert new words to the dictionary
+ *
+ * @param p is a pointer to game_goes_on variable in start_new_game method
+ * @return 1 if this method has to be called again, 0 if not
+ */
 int manage_input_new_game(int *p) {
     char *ins = malloc(18);
 
@@ -247,4 +318,24 @@ int manage_input_new_game(int *p) {
         free(ins);
         return 1;
     }
+}
+
+/**
+ * The method checks if the param word belongs to the list that represent the dictionary or not
+ *
+ * @param word is the word inserted by user in input to try to guess the reference word
+ * @return 1 if the word belongs to the dictionary, 0 if not
+ */
+int check_if_belongs_to_the_dictionary(char *word) {
+    Node *tmp = dictionary_head;
+
+    while(tmp != NULL) {
+        if (strcmp(word, tmp->word) == 0) {
+            return 1;
+        }
+
+        tmp = tmp->next;
+    }
+
+    return 0;
 }
